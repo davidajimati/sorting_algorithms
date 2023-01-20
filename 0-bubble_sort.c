9fed4454ca0ecf3a2325ff6314a87ebc5d237a29
davidajimati <ajimatidavid@gmail.com>
2023-01-20 09:12:43 +0100

    #FIX: limit applied to loop

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,7 +17,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			now = array[j];
 			next = array[j + 1];
