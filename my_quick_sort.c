bc024c18c83013f4213aa3bb7afd362b27f4b8b8
davidajimati <ajimatidavid@gmail.com>
2023-01-25 15:02:57 +0100

    #FIX: bugs overflow

--- my_quick_sort.c
+++ my_quick_sort.c
@@ -1,8 +1,10 @@
 #include "sort.h"
+#include <stdlib.h>
 
 void sort_engine(int *array, int b, int ub, size_t size);
 void swap(int *array, int loc_a, int loc_b);
 int lomuto_partition(int *array, int lb, int ub, size_t size);
+int pivot_find(size_t size);
 
 /**
  * quick_sort - sorting algorithm
@@ -14,48 +16,12 @@ int lomuto_partition(int *array, int lb, int ub, size_t size);
 
 void quick_sort(int *array, size_t size)
 {
-	int lower = 0;
-	int upper = size - 1;
+	int lb = 0;
 
 	if (!array || size < 2)
 		return;
 
-	sort_engine(array, lower, upper, size);
-}
-
-/**
- * lomuto_partition - implements partition and finds pivot
- *
- * @array: target
- * @lb: lower bound
- * @ub: upper bound
- * @size: of array
- * Return: Pivot
- */
-
-int lomuto_partition(int *array, int lb, int ub, size_t size)
-{
-	int pivot = array[lb];
-	int start = lb;
-	int end = ub;
-
-	while (start < end)
-	{
-		while (array[start] <= pivot)
-			start++;
-
-		while (array[end] > pivot)
-			end--;
-
-		if (start < end)
-		{
-			swap(array, start, end);
-			print_array(array, size);
-		}
-	}
-	swap(array, pivot, end);
-	print_array(array, size);
-	return (end);
+	sort_engine(array, lb, size - 1, size);
 }
 
 /**
@@ -71,13 +37,48 @@ void sort_engine(int *array, int lb, int ub, size_t size)
 {
 	int loc;
 
-	sort_engine(array, lb, ub, size);
+	if (lb >= ub)
+		return;
+
+	loc = lomuto_partition(array, lb, ub, size);
 	if (lb < ub)
 	{
-		loc = lomuto_partition(array, lb, ub, size);
 		sort_engine(array, lb, loc - 1, size);
 		sort_engine(array, loc + 1, ub, size);
 	}
+}
+
+/**
+ * lomuto_partition - implements partition and finds pivot
+ *
+ * @array: target
+ * @lb: lower bound
+ * @ub: upper bound
+ * @size: of array
+ * Return: Pivot
+ */
+
+int lomuto_partition(int *array, int lb, int ub, size_t size)
+{
+	int start = lb;
+	int end = ub;
+	int pl = pivot_find(size);
+	int pivot = array[pl];
+
+	while (start < end)
+	{
+		for (; array[start] <= pivot; start++);
+		for (; array[ub] > pivot; end--);
+
+		if (start != pl && end != pl)
+		{
+			swap(array, start, end);
+			print_array(array, size);
+		}
+	}
+
+	swap(array, pl, end);
+	return (end);
 }
 
 /**
@@ -93,4 +94,17 @@ void swap(int *array, int loc_a, int loc_b)
 
 	array[loc_a] = array[loc_b];
 	array[loc_b] = temp;
+}
+
+/**
+ * pivot_find - find the location of the pivot in the array
+ *
+ * @size: of array
+ * Return: position of pivot
+*/
+int pivot_find(size_t size)
+{
+	int pl = rand() % size;
+
+	return (pl);
 }
